e2497c89a2359b8e4d7df8c89356a296fee59907
Conor Byrne <71222289+dreamhopping@users.noreply.github.com>
2021-05-30 18:51:00 +0100

    [Build] Update JDA to 0feb089

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,12 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://m2.dv8tion.net/releases")
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:4.2.1_265")
+    // implementation("net.dv8tion:JDA:4.2.1_265")
+    implementation("com.github.DV8FromTheWorld:JDA:0feb089")
     implementation("com.sedmelluq:lavaplayer:1.3.77")
 
     implementation("org.apache.logging.log4j:log4j-api:2.8")
