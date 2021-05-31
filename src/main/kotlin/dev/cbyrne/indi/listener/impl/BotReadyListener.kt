bd4163426d686189b654d55e2936ce36711c648f
Conor Byrne <71222289+dreamhopping@users.noreply.github.com>
2021-05-31 10:02:48 +0100

    [Listeners] Remove invite url logging

--- src/main/kotlin/dev/cbyrne/indi/listener/impl/BotReadyListener.kt
+++ src/main/kotlin/dev/cbyrne/indi/listener/impl/BotReadyListener.kt
@@ -9,8 +9,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 class BotReadyListener : ListenerAdapter() {
     override fun onReady(event: ReadyEvent) {
         Indi.logger.info("Ready!")
-
         event.jda.presence.activity = Activity.listening("your commands | Prefix: \"${Configuration.prefix}\"")
-        Indi.logger.info("Invite URL: ${event.jda.getInviteUrl(Indi.defaultPermissions)}")
     }
 }
